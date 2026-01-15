f9b421b3852c47ff5894dca3e50c80750762b9d4
Sadiya Khan <144613374+sadiyakhan2004@users.noreply.github.com>
2026-01-16 01:45:31 +0530

    fixing bugs

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -63,7 +63,7 @@ const Product = () => {
             <p>Select Size</p>
             <div className="flex gap-2">
               {productData.sizes.map((item,index)=>(
-                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item == size ? 'border-orange-500' : "" }`} key={index}> {item}</button>
+                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500' : "" }`} key={index}> {item}</button>
               ))}
             </div>
           </div>
